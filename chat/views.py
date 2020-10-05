0806424e17ef35c477307ef31b3efc77bd170af0
LHS <gudtjr121@naver.com>
2020-10-05 16:37:27 +0900

    chat2

--- chat/views.py
+++ chat/views.py
@@ -3,16 +3,19 @@ from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
 
+#테스트
 def index(request):
-    context = {'team_list' : ['ai','good']}
+    context = {
+        'team_list' : ['팀메이트','멋쟁이사자처럼','솔트룩스','에이림'],
+        }
 
     return render(request, 'chat/index.html', context)
 
 def room(request, room_name):
     user = request.user
     context = {
-        'room_name_json': mark_safe(json.dumps(room_name))
-
+        'room_name_json': mark_safe(json.dumps(room_name)),
+        'room_name':room_name,
     }
 
     return render(request, 'chat/room.html', context)
