dff08a223da6f740c9be5067883810d687fab0b5
LHS <gudtjr121@naver.com>
2020-10-06 12:19:24 +0900

    chat

--- chat/views.py
+++ chat/views.py
@@ -2,11 +2,17 @@
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
+from team_rec.models import Team_list, Team_member
 
 #테스트
 def index(request):
+    team_list = Team_list.objects.all
+    team_member = Team_member.objects.all
+
+
     context = {
-        'team_list' : ['팀메이트','멋쟁이사자처럼','솔트룩스','에이림'],
+        'team_list' : team_list,
+        'team_member' : team_member
         }
 
     return render(request, 'chat/index.html', context)
