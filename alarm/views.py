9dc5451807225c14a65dbe5d4c72683c4681cae4
inuinseoul <70463738+inuinseoul@users.noreply.github.com>
2020-10-06 15:03:47 +0900

    중복가입방지 + 에러메세지

--- alarm/views.py
+++ alarm/views.py
@@ -53,12 +53,9 @@ def check_info(request, message_pk):
     return render(request, "alarm/check_info.html", context)
 
 def join_team(request, message_pk):
-    message = Message.objects.get(pk=message_pk)
+    message = Message.objects.get(pk=message_pk)    
     team = Team_list.objects.get(pk=message.invite_team.pk)
-    Team_member.objects.create(
-        team_id=team,
-        member=request.user
-    )
+
     customer = message.recipient
     message.delete()
 
@@ -68,6 +65,18 @@ def join_team(request, message_pk):
     context = {
         "message_list_team": message_list_team,
         "message_list_study": message_list_study,
+        "error_state" : False
     }
+
+    if not (Team_member.objects.filter(team_id=team).filter(member=request.user)):
+        Team_member.objects.create(
+            team_id=team,
+            member=request.user
+        )
+        context["error_state"] = True
+        context["error_message"] = team.name + "팀에 가입되었습니다."
+    else:
+        context["error_state"] = True
+        context["error_message"] = "이미 해당 팀에 가입하셨거나 해당 팀이 존재하지 않습니다."
 
     return render(request, "alarm/alarm_list.html", context)
